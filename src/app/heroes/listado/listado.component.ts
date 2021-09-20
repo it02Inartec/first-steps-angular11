2138cd7c5ebd050ebb4f4e82b73b1e1a46051477
Jennifer Miranda <jennifer.beuses@gmail.com>
2021-09-20 12:19:59 -0400

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    // this.heroes.splice(1, 1);
+    this.heroeBorrado = this.heroes.shift() || '';
+    console.log(this.heroeBorrado);
+  }
+}
